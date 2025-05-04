785a57b7183e483ef726368f05ae9a3de508573f
Renan Santana <renanzintestes@gmail.com>
2025-05-04 09:10:52 -0300

    changed the type of status in room interface to better integration with the logic, and added the condition in case where roomis empty

--- src/interfaces/room-interface.ts
+++ src/interfaces/room-interface.ts
@@ -1,9 +1,17 @@
+enum Status {
+  AVAILABLE = "AVAILABLE",
+  IN_USE = "IN_USE",
+  MAINTENANCE = "MAINTENANCE",
+  INTERDICTED = "INTERDICTED",
+  UNAVAILABLE = "UNAVAILABLE",
+}
+
 export interface Room {
   id: string,
   name: string,
   description: string,
   location: string,
-  status: string,
+  status: Status,
   createdAt: Date,
   updatedAt: Date,
 }
--- src/services/rooms/find-room-service.ts
+++ src/services/rooms/find-room-service.ts
@@ -1,30 +1,22 @@
-import { RoomStatus } from "@prisma/client";
 import { prisma } from "../../config/prisma";
 import { Room } from "../../interfaces/room-interface";
 import { logger } from "../../utils/logger";
 
 export class FindRoomService {
   async execute({ status }: Pick<Room, 'status'>) {
-
-    try {
-      let roomStatus: RoomStatus | undefined
-
-      if (status) {
-        if (Object.values(RoomStatus).includes(status as RoomStatus)) {
-          roomStatus = status as RoomStatus
-        }
+    const roomsFinded = await prisma.rooms.findMany({
+      where: {
+        status: status
       }
+    })
 
-      const roomsFinded = await prisma.rooms.findMany({
-        where: {
-          status: roomStatus
-        }
-      })
+    console.log(roomsFinded)
 
-      return roomsFinded
-    } catch (error) {
-      logger.error(`Error on find room(s) on the database: ${error}`)
-      throw new Error(`Error on find room(s) on the database: ${error}`)
+    if (roomsFinded.length === 0) {
+      logger.warn("Room(s) not found")
+      throw new Error("Room(s) not found")
     }
+
+    return roomsFinded
   }
 }
